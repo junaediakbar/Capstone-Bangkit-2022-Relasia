e1b77ceb1276c8a3156b80e2cdfc10681d637627
naimackerman <naimackerman@gmail.com>
2022-05-25 02:51:56 +0700

    finish : complete UI layout for Helper side

--- Mobile Development/RelasiaHelperApp/app/src/main/java/com/c22ps099/relasiahelperapp/view/BaseActivity.kt
+++ Mobile Development/RelasiaHelperApp/app/src/main/java/com/c22ps099/relasiahelperapp/view/BaseActivity.kt
@@ -1,11 +1,11 @@
-package com.c22ps099.relasiahelperapp
+package com.c22ps099.relasiahelperapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
     }
 }
