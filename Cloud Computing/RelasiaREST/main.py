ed7750265f170056f6bf5742bf545ec396e26ce1
fthrr <fathurrahman.irwansa@ui.ac.id>
2022-05-27 20:41:45 +0700

    Threaded Main.py

--- Cloud Computing/RelasiaREST/main.py
+++ Cloud Computing/RelasiaREST/main.py
@@ -1,9 +1,11 @@
 from API import create_app
 app = create_app()
 
+
 @app.route('/')
 def index():
     return "<h1>Relasia API</h1>"
 
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=True)
