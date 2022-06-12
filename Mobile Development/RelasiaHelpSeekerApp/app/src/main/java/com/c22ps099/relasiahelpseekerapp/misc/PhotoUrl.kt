2e2099444a930cc432318e13a0d73b894d36c8b1
Junaedi Akbar <juned.akb@gmail.com>
2022-06-12 22:57:24 +0700

    clean: ui bug

--- Mobile Development/RelasiaHelpSeekerApp/app/src/main/java/com/c22ps099/relasiahelpseekerapp/misc/PhotoUrl.kt
+++
@@ -1,8 +0,0 @@
-package com.c22ps099.relasiahelpseekerapp.misc
-
-import com.google.gson.annotations.SerializedName
-
-data class StringPhotoUrl (
-    @SerializedName("file")
-    val file : String? =""
-        )
--- Mobile Development/RelasiaHelpSeekerApp/app/src/main/java/com/c22ps099/relasiahelpseekerapp/ui/form/DialogFormSuccess.kt
+++
@@ -1,4 +0,0 @@
-package com.c22ps099.relasiahelpseekerapp.ui.form
-
-class DialogFormSuccess {
-}
